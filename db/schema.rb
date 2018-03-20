d578a0899cedfa8fec38643f0c41b9ccf90f4dd5
aaronsato <2asato@gmail.com>
2018-03-19 23:30:58 -0700

    changed description to details

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180320061722) do
+ActiveRecord::Schema.define(version: 20180320062945) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 20180320061722) do
     t.string "title"
     t.boolean "completed"
     t.integer "user_id"
-    t.string "description"
+    t.string "details"
   end
 
   create_table "users", force: :cascade do |t|
